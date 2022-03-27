02f877368e4d7efb1de955d432c9b8a9a62da557
Kieran R <kieranrutter2002@outlook.com>
2022-03-27 13:48:46 +0100

    Subplots
    4 plots are now displayed on the screen at once (3 are the same at the moment) the other one is the elbow

--- ClusteringAI.py
+++ ClusteringAI.py
@@ -11,6 +11,9 @@ import seaborn as sns
 #sets the style of the grid
 sns.set_style("darkgrid")
 
+#Sets the subplots
+fig, ([ax1, ax2],[ax3, ax4]) = plt.subplots(2,2, figsize=(16,6))
+
 #Loads the dataset
 X = pd.read_csv('Data-Cluster.csv')
 X.head()
@@ -24,11 +27,11 @@ for k in K:
     distortions.append(kmeanModel.inertia_)
 
 plt.figure(figsize=(16,8))
-plt.plot(K, distortions, 'bx-')
-plt.xlabel('k')
-plt.ylabel('Distortion')
-plt.title('The Elbow Method showing the optimal k')
-plt.show()
+ax1.plot(K, distortions, 'bx-')
+ax1.set_xlabel('k')
+ax1.set_ylabel('Distortion')
+ax1.set_title('The Elbow Method showing the optimal k')
+
 
 #Assigns collumns
 X = X.filter(["fnlwgt", "age" ], axis = 1)
@@ -37,7 +40,18 @@ model = KMeans(n_clusters= 5)
 model.fit(X)
 
 #plot the cluster
-sns.scatterplot(data = X, x="fnlwgt", y= "age", c= model.labels_, cmap= 'rainbow' )
+sns.scatterplot(data = X, x="fnlwgt", y= "age", c= model.labels_, cmap= 'rainbow', ax=ax2)
 #Plots cluster mid points
-sns.scatterplot(x=model.cluster_centers_[:, 0], y=model.cluster_centers_[:, 1], c=['black'])
+sns.scatterplot(x=model.cluster_centers_[:, 0], y=model.cluster_centers_[:, 1], c=['black'], ax=ax2)
+
+#plot the cluster
+sns.scatterplot(data = X, x="fnlwgt", y= "age", c= model.labels_, cmap= 'rainbow', ax=ax3)
+#Plots cluster mid points
+sns.scatterplot(x=model.cluster_centers_[:, 0], y=model.cluster_centers_[:, 1], c=['black'], ax=ax3)
+
+#plot the cluster
+sns.scatterplot(data = X, x="fnlwgt", y= "age", c= model.labels_, cmap= 'rainbow', ax=ax4)
+#Plots cluster mid points
+sns.scatterplot(x=model.cluster_centers_[:, 0], y=model.cluster_centers_[:, 1], c=['black'], ax=ax4)
+
 plt.show()
